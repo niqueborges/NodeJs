bca12794416803de67a3ddb34e88b43cc69cdf4e
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-14 14:05:58 -0300

    Feat: Aula NodeJs

---
+++ index.js
@@ -0,0 +1,5 @@
+const {Person} = require('./person'); // Import the Person class from person
+
+const person = new Person('Alice'); // Create a new instance of Person
+
+console.log(person.sayMyName()); // My name is Alice!
